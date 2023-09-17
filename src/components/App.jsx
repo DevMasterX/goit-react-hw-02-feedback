a40983b09b6cef8d59dbbdf1b0ee6dee06e40713
DevMasterX <sokol080288@gmail.com>
2023-09-17 21:50:42 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,64 @@
+import { Component } from "react";
 
-class App extends Component {
-  state = {  good: 0,
-    neutral: 0,
+export class App extends Component {
+  state = { 
+    good: 10,
+    neutral: 10,
     bad: 0 } 
+
+    countTotalFeedback=()=>{
+      return this.state.good + this.state.neutral+this.state.bad
+    }; 
+
+    countPositiveFeedbackPercentage=()=> {
+      return Math.round((this.state.good / this.countTotalFeedback())*100);
+    }
+
   render() { 
+    const{good, neutral, bad} = this.state;
+    const total = this.countTotalFeedback();
+    const percentage = this.countPositiveFeedbackPercentage();
     return (
-      <div></div>
+      <div>
+
+ <h2>Please leave feedback</h2>
+
+<ul>
+  <li><button>Good</button></li>
+   <li><button>Neutral</button></li>
+  <li><button>Bad</button></li>
+ </ul>
+ <h2>Statistics</h2>
+ <ul>
+ <li>Good: {good}</li>
+   <li>Neutral: {neutral}</li>
+  <li>Bad: {bad}</li>
+  <li>Total: {total}</li>
+  <li>Positive feedback: {percentage}%</li>
+ </ul>
+
+      </div>
     );
   }
 }
  
-export default App;
 
 
 
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
+
+// <h2>Please leave feedback</h2>
+
+// <ul>
+//   <li><button>Good</button></li>
+//   <li><button>Neutral</button></li>
+//   <li><button>Bad</button></li>
+// </ul>
+// <h2>Statistics</h2>
+// <ul>
+//   <li>Good: 0</li>
+//   <li>Neutral: 0</li>
+//   <li>Bad: 0</li>
+//   <li>Total: 0</li>
+//   <li>Positive feedback: 0%</li>
+// </ul>
