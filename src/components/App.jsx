5ede00efdc1281f9231f043fbab6c682746470a0
DevMasterX <sokol080288@gmail.com>
2023-09-17 20:07:15 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+class App extends Component {
+  state = {  good: 0,
+    neutral: 0,
+    bad: 0 } 
+  render() { 
+    return (
+      <div></div>
+    );
+  }
+}
+ 
+export default App;
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
