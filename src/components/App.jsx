bef205a137f542eab69b55bfefe6d5b7617577c0
DevMasterX <sokol080288@gmail.com>
2023-09-18 01:40:49 +0300

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,75 +1,62 @@
-import { Component } from "react";
+import { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
 export class App extends Component {
-  state = { 
+  state = {
     good: 0,
     neutral: 0,
-    bad: 0 } 
+    bad: 0,
+  };
 
-    // handleClickFeedbackBtn =({target}) =>{
-    //   console.log(target)
-    //   const feedback = target.textContent.toLowerCase();
-    //   this.setState((prevState) => {
-    //     return{
-    //       [feedback]: prevState[feedback] +1
-    //     }
-    //   })
-    // }
+  handleClickFeedbackBtn = feedback => {
+    this.setState(prevState => ({
+      [feedback]: prevState[feedback] + 1,
+    }));
+  };
 
-    handleClickFeedbackBtn = (feedback) => {
-      this.setState((prevState) => ({
-        [feedback]: prevState[feedback] + 1,
-      }));
-    };
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
 
-    
-    
-    
-    
-    
-    
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
 
-
-    countTotalFeedback=()=>{
-      return this.state.good + this.state.neutral+this.state.bad
-    }; 
-
-    countPositiveFeedbackPercentage=()=> {
-      return Math.round((this.state.good / this.countTotalFeedback())*100);
-    }
-
-  render() { 
-    const{good, neutral, bad} = this.state;
+  render() {
+    const { good, neutral, bad } = this.state;
     const total = this.countTotalFeedback();
     const positivePercentage = this.countPositiveFeedbackPercentage();
     return (
-      <div>
+      <>
 
- <h2>Please leave feedback</h2>
+<Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.handleClickFeedbackBtn}
+        />
 
-<ul>
-  <li><button onClick={() => this.handleClickFeedbackBtn('good')}>Good</button></li>
-   <li><button onClick={() => this.handleClickFeedbackBtn('neutral')}>Neutral</button></li>
-  <li><button onClick={() => this.handleClickFeedbackBtn('bad')}>Bad</button></li>
- </ul>
- <h2>Statistics</h2>
- <ul>
- <li>Good: {good}</li>
-   <li>Neutral: {neutral}</li>
-  <li>Bad: {bad}</li>
-  <li>Total: {total}</li>
-  <li>Positive feedback: {positivePercentage}%</li>
- </ul>
-
-      </div>
+</Section>
+         <Section title="Statistics">
+         {total ?
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        
+        : 
+        <Notification message="There is no feedback" />
+        }
+        </Section>
+      </>
     );
   }
 }
- 
-
-
-
-
 
 // <h2>Please leave feedback</h2>
 
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+import css from './Notification.module.css'
+
+const Notification = ({message}) => {
+  return (
+    <p className={css.message}>
+      {message}
+    </p>
+  )
+}
+
+export default Notification
